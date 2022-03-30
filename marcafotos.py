4a4aaccb6d72a0d7f69c828de30271aee6e49a1d
Eduardo Garre <eduardo.garre@gmail.com>
2022-03-30 10:56:47 +0200

    Encapsulo el proceso de marca en una función

--- marcafotos.py
+++ marcafotos.py
@@ -4,33 +4,38 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-tamaño_marca = (300, 100)
-posición_marca = (100, 100)
+def marcafotos():
 
-# Abro foto
-foto_original = Image.open("imagen.png")
-foto = foto_original.convert("RGBA")
+    tamaño_marca = (300, 100)
+    posición_marca = (100, 100)
 
-# Abro la marca
-marca_original = Image.open("marca.png")
-marca = marca_original.convert("RGBA")
+    # Abro foto
+    foto_original = Image.open("imagen.png")
+    foto = foto_original.convert("RGBA")
 
-# Cambio el tamaño de la marca
-marca.thumbnail(tamaño_marca)
+    # Abro la marca
+    marca_original = Image.open("marca.png")
+    marca = marca_original.convert("RGBA")
 
-# Creo 2 máscara del tamaño de la foto
-máscara1 = Image.new("RGBA", foto.size)
-máscara2 = Image.new("RGBA", foto.size)
+    # Cambio el tamaño de la marca
+    marca.thumbnail(tamaño_marca)
 
-# Pego la marca sobre la primera máscara
-máscara1.paste(marca, posición_marca)
+    # Creo 2 máscara del tamaño de la foto
+    máscara1 = Image.new("RGBA", foto.size)
+    máscara2 = Image.new("RGBA", foto.size)
 
-# Construyo la máscara final, ajustando transparencia
-máscara = Image.blend(máscara1, máscara2, 0.9)
+    # Pego la marca sobre la primera máscara
+    máscara1.paste(marca, posición_marca)
 
-# Pego la máscara sobre la foto
-foto_final = Image.alpha_composite(foto, máscara)
+    # Construyo la máscara final, ajustando transparencia
+    máscara = Image.blend(máscara1, máscara2, 0.9)
 
-# Muestro el resultado
-plt.imshow(foto_final)
-plt.show()
+    # Pego la máscara sobre la foto
+    foto_final = Image.alpha_composite(foto, máscara)
+
+    # Muestro el resultado
+    plt.imshow(foto_final)
+    plt.show()
+
+if __name__ == "__main__":
+    marcafotos()
